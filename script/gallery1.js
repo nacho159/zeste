30aa98c7569f3b3ba40d4b75373504bade8fa95a
nacho159 <96101853+nacho159@users.noreply.github.com>
2022-01-28 14:37:49 +0900

    Add files via upload

---
+++ script/gallery1.js
@@ -0,0 +1,47 @@
+$(document).on('ready', function () {
+
+  $(".slider").slick({
+    //autoplay:true,자동 갤러리 구문(삭제시 수동으로 변함.)
+    //autoplaySpeed:2000,자동 갤러리가 넘어가는 시간
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,//보여지는 갤러리 수
+    slidesToScroll: 1,//넘어가는 갤러리 수
+    speed: 2000,//다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간
+    pauseOnHover: false, /* 마우스 호버시 슬라이드 이동 멈춤 */
+    responsive: [
+      {
+        breakpoint: 601,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        
+        }
+      }
+    ]
+  });
+
+  $(".slider2").slick({
+    //autoplay:true,자동 갤러리 구문(삭제시 수동으로 변함.)
+    //autoplaySpeed:2000,자동 갤러리가 넘어가는 시간
+    dots: false,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 2,//보여지는 갤러리 수
+    slidesToScroll: 1,//넘어가는 갤러리 수
+    speed: 1000,//다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간
+    pauseOnHover: false, /* 마우스 호버시 슬라이드 이동 멈춤 */
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 601,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        
+        }
+      }
+    ]
+  });
+
+});
